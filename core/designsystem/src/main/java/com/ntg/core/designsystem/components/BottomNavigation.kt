9375cbad7a6950a48d0385d19b89235401c2478c
NTGAli <alintg@outlook.com>
2024-08-07 18:28:08 +0330

    Fix height bottom navigation

--- core/designsystem/src/main/java/com/ntg/core/designsystem/components/BottomNavigation.kt
+++ core/designsystem/src/main/java/com/ntg/core/designsystem/components/BottomNavigation.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -64,6 +66,7 @@ fun BottomNavigation(
 
     Column(
         modifier = modifier
+            .height(IntrinsicSize.Min)
             .background(MaterialTheme.colorScheme.background)
     ) {
         HorizontalDivider(color = MaterialTheme.colorScheme.surfaceDim)
