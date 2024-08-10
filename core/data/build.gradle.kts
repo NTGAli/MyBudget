b320cec865f11fe34f5a073da893a3a3493bb6e3
NTGAli <alintg@outlook.com>
2024-08-10 10:20:45 +0330

    Add data module

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    alias(libs.plugins.budgetapp.android.library)
+    alias(libs.plugins.budgetapp.android.library.jacoco)
+    alias(libs.plugins.budgetapp.android.hilt)
+    id("kotlinx-serialization")
+}
+
+android {
+    namespace = "com.ntg.core.data"
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":core:database"))
+    implementation(project(":core:common"))
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.kotlinx.serialization.json)
+}
