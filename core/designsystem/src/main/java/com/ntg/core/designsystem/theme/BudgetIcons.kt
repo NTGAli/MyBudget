842ac28c4e4c86bedc78e8f123ff50df8014b542
NTGAli <alintg@outlook.com>
2024-08-11 12:04:43 +0330

    Implement bank card component

--- core/designsystem/src/main/java/com/ntg/core/designsystem/theme/BudgetIcons.kt
+++ core/designsystem/src/main/java/com/ntg/core/designsystem/theme/BudgetIcons.kt
@@ -31,4 +31,6 @@ object BudgetIcons {
   val CameraPlus = R.drawable.cameraplus_24
   val Tick = R.drawable.check_small
   val Add = R.drawable.add
+  val bankCard = R.drawable.bank_card
+  val chip = R.drawable.emv_chip
 }
