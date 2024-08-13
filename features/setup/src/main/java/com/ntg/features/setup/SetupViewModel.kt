731dce97eaddab2a5b8d490eb41006dd5944828c
NTGAli <alintg@outlook.com>
2024-08-13 06:39:26 +0330

    Add insert source in viewModel

--- features/setup/src/main/java/com/ntg/features/setup/SetupViewModel.kt
+++ features/setup/src/main/java/com/ntg/features/setup/SetupViewModel.kt
@@ -3,7 +3,9 @@ package com.ntg.features.setup
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ntg.core.data.repository.AccountRepository
+import com.ntg.core.data.repository.SourceExpenditureRepository
 import com.ntg.core.model.Account
+import com.ntg.core.model.SourceExpenditure
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -11,7 +13,8 @@ import javax.inject.Inject
 @HiltViewModel
 class SetupViewModel
 @Inject constructor(
-    private val accountRepository: AccountRepository
+    private val accountRepository: AccountRepository,
+    private val sourceRepository: SourceExpenditureRepository
 ) : ViewModel() {
 
     fun accounts() = accountRepository.getAll()
@@ -21,6 +24,15 @@ class SetupViewModel
     ){
         viewModelScope.launch {
             accountRepository.insert(account)
+        }
+    }
+
+
+    fun insertNewSource(
+        source: SourceExpenditure
+    ){
+        viewModelScope.launch {
+            sourceRepository.insert(source)
         }
     }
 
