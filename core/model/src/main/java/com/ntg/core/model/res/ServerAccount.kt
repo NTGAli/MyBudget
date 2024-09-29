277b5bf28abd7082a713c113ee998e679f30bb91
NTGAli <alintg@outlook.com>
2024-09-29 07:05:44 +0330

    Add dataclass for wallet

--- core/model/src/main/java/com/ntg/core/model/res/ServerAccount.kt
+++ core/model/src/main/java/com/ntg/core/model/res/ServerAccount.kt
@@ -13,5 +13,30 @@ data class ServerAccount(
     val updatedAt: String? = null,
     @SerializedName("deleted_at")
     val deletedAt: String? = null,
+    val wallets: List<WalletRes>? = null,
+)
+
+
+data class WalletRes(
+    val id: String? = null,
+    @SerializedName("wallet_type_id")
+    val walletType:Int? = null,
+    @SerializedName("currency_id")
+    val currencyId: Int,
+    @SerializedName("account_id")
+    val accountId: String? = null,
+    val details: WalletDetailsRes,
+    )
+
+data class WalletDetailsRes(
+    @SerializedName("bank_id")
+    val bankId: Int? = null,
+    @SerializedName("bank_name")
+    val bankName: String?= null,
+    @SerializedName("cart_number")
+    val cardNumber: String? = null,
+    @SerializedName("cart_owner_name")
+    val cardOwner: String? = null,
+    val cvv2: String? = null,
 
 )
