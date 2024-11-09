f8c607770f375940b150355679f83ad3516a9a91
NTGAli <alintg@outlook.com>
2024-11-09 14:58:50 +0330

    update full screen bottom sheet

--- core/designsystem/src/main/java/com/ntg/core/designsystem/components/FullScreenBottomSheet.kt
+++ core/designsystem/src/main/java/com/ntg/core/designsystem/components/FullScreenBottomSheet.kt
@@ -6,21 +6,28 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FullScreenBottomSheet(
     showSheet: MutableState<Boolean>,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
     appbar:@Composable () -> Unit,
     content:@Composable () -> Unit,
 ) {
@@ -39,7 +46,12 @@ fun FullScreenBottomSheet(
 
         Scaffold(
             topBar = {
-                appbar()
+                Column {
+                    appbar()
+                    if ((scrollBehavior?.state?.contentOffset ?: 0f) < -25f) {
+                        HorizontalDivider(Modifier.height(1.dp), color = MaterialTheme.colorScheme.surfaceContainerHighest)
+                    }
+                }
             }
         ) {
             Box(
